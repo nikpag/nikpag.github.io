3111b6d692f46615506d2c9e2aecf0164850ab62
nikpag <nikospagonas00@gmail.com>
2026-05-30 14:09:26 -0700

    Make news scrollbar always visible, rearrange paper order in cv

--- assets/js/common.js
+++ assets/js/common.js
@@ -45,5 +45,48 @@ $(document).ready(function() {
       });
     }
   });
-});
 
+  const updateNewsScrollbar = function($scroller) {
+    const scroller = $scroller.get(0);
+    const $indicator = $scroller.siblings(".news-scrollbar-indicator");
+    const $thumb = $indicator.find("span");
+
+    if (!scroller || !$thumb.length) {
+      return;
+    }
+
+    const scrollHeight = scroller.scrollHeight;
+    const clientHeight = scroller.clientHeight;
+
+    if (scrollHeight <= clientHeight) {
+      $indicator.hide();
+      return;
+    }
+
+    $indicator.show();
+    const trackHeight = $indicator.height();
+    const thumbHeight = Math.max(Math.round(trackHeight * clientHeight / scrollHeight), 32);
+    const maxThumbTop = trackHeight - thumbHeight;
+    const maxScrollTop = scrollHeight - clientHeight;
+    const thumbTop = maxScrollTop > 0 ? Math.round(scroller.scrollTop / maxScrollTop * maxThumbTop) : 0;
+
+    $thumb.css({
+      height: thumbHeight + "px",
+      transform: "translateY(" + thumbTop + "px)",
+    });
+  };
+
+  const $newsScrollers = $(".news-scrollable");
+  $newsScrollers.each(function() {
+    const $scroller = $(this);
+    updateNewsScrollbar($scroller);
+    $scroller.on("scroll", function() {
+      updateNewsScrollbar($scroller);
+    });
+  });
+  $(window).on("load resize", function() {
+    $newsScrollers.each(function() {
+      updateNewsScrollbar($(this));
+    });
+  });
+});
